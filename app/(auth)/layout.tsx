7a9ecea1b83dc9f5948c1fa5a6f76caf85815baf
Hansraj Saini <hansraj@instient.com>
2024-09-15 14:39:13 +0530

    authentication added using clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='relative flex h-screen w-full flex-col items-center justify-center'> 
+      <div className='mt-12'>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
